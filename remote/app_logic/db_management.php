2ad43e41818155757e7db049b7483646c6337bda
Stefano Cieri <linfa.lab@gmail.com>
2012-12-01 11:41:07 +0100

    DB connect

--- remote/app_logic/db_management.php
+++ remote/app_logic/db_management.php
@@ -4,8 +4,32 @@ require_once("db_info.php");
 
 function dbConnect(){
 
+$con = mysql_connect($dbHost,$dbUser,$dbPassword);
+if(!$con){ die('Could not connect: ' . mysql_error()); }
 
+mysql_select_db($dbName,$con);;
 
+echo 'Connected successfully';
+return $con;
+
+}
+
+/*
+function dbClose($con){
+	
+}
+*/
+
+function dbQuery($q){
+
+	$con = $dbConnect();
+
+	mysql_query($q);
+	while($row = mysql_fetch_array()){
+		print $row;
+	}
+	
+	mysql_close($con);
 }
 
 ?>
