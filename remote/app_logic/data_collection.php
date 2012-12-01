7a6044baa6f884e32317d009c4b8574658e09df5
Stefano Cieri <linfa.lab@gmail.com>
2012-12-01 20:56:37 +0100

    SmartMeter PARSER + DB writing

--- remote/app_logic/data_collection.php
+++ remote/app_logic/data_collection.php
@@ -2,95 +2,157 @@
 
 require_once("db_management.php");
 
-$path = '20121106_2166_cdc_grezze_Domestico.csv';
+$path = '../src/20121106_2166_cdc_grezze_Domestico.csv';
 // $path = '20121106_2162_cdc_grezze_Produttore.csv';
 
-function stringToTime($str,$mode){
-	if($mode=='date'){
-		echo $str.' : date';
-	}else if($mode=='time'){
-		echo $str.' : time';
+
+
+// PARSE SMART METER
+
+
+ /*
+function stringToTime($str){
+	$str = (int)end(explode('_',$str));
+	$hour = floor($str*15/60);	// hard coded by now
+	$min = ($str*15)%60;		// hard coded by now
+	return array($hour,$min);
+}
+*/
+
+function stringToTime($mode,$str,$timeArray=array(0,0)){
+	switch($mode){
+		case 'date':
+			// print_r($timeArray);
+			$date = explode('/',$str);
+			$date = date(mktime($timeArray[0],$timeArray[1],0,(int)$date[1],(int)$date[0],(int)$date[2]));
+			//$date = ($formatted)? date('d/M/Y',$date) : $date;
+			if ($timeArray==array(0,0)){
+				$date = date('d/M/Y',$date);
+			}else{
+				$date = date('Y-m-d H:i:s',$date);
+			}
+			return $date;
+		case 'time':
+			$tmp = explode('_',$str);
+			$str = (int)(end($tmp));
+			$hour = ($str*15/60);		// hard coded by now
+			$min = ($str*15)%60;		// hard coded by now
+			return array($hour,$min);
+		default:
+			return $str.' : default';
 	}
 	return $str;
 }
 
-function stringToDatetime(){
-	echo 'yeah';
-}
-
-// Parse Smart Meter
 
 function convertSmData($key,$str){
+	
 	//convert smart meter data
 	$eType = array(
 				'146'=>'Attiva Prelevata',		//	'Prelevata (at)'
 				'147'=>'Attiva Immessa',		//	'Immessa (at)'
 				'148'=>'Reattiva 1° Quadrante'	//	'I° Quadrante (re)'
 			); 
-
+	
 	switch ($key) {
 		case 'GIORNO_CDC':
 			//var_dump($date);
-			return stringToTime($str,'date');
+			return stringToTime('date',$str);
 		//case 'PROGRESSIVO':
 		//	return $prog[$str];
-			return $eType[$str];
 		case 'ID_OBIS':
+			return $eType[$str];
 		case 'ID_PUNTO_MISURA':
 			return (int)$str;
 		default:
 			print $key;
 			return $str;
 	}
+	
 }
 
-parseData($path,$type=null){
+
+function parseData($path,$type=null){
 
 	if($type==null){
-		
+	
 		$dailyRecs = 96;
 		$data = array();
 		
 		//file open
-		$file = fopen($path,"r")
+		$file = fopen($path,"r");
+		
+		$header = true;
+		
 		while (!feof($file)) {
 			$line = fgets($file);
 			$rawData = explode(';', $line);
-			
-			if(count($rawData)!=196) print count($lineData).' :: ';
-			
-			for ($i=0; $i<$dailyRecs; $i++){
-			
-				$pointer = 4 + $i * 2;
-				$id = (int)$lineData[3];
+			// if(count($rawData)!=196) print count($rawData).' :: ';
+			//var_dump($rawData);
+			if ($header){
+				$keys = explode(';',$line);
+				$header = false;
+			}else{			
+				for ($i=0; $i<$dailyRecs; $i++){
 				
-				$type = 
-				
-				$nRec = $keys[$pointer];
-				$hourMin = stringToTime($nRec,'time');
-				$datetime = stringToTime($lineData[0],$hourMin,true);
-				
-				$val = (float)str_replace(',','.',$lineData[$pointer]);
-				$cState = (float)str_replace(',','.',$lineData[$pointer+1]);
-				
-				$parsedData = array(
-					'id'=>$id,
-					'time'=>$datetime,
-					'type'=>$type,
-					'value'=>$val,
-					'cState'=>$cState
+					$pointer = 4 + $i * 2;
+					$id = (int)$rawData[3];
+					
+					$type = convertSmData('ID_OBIS',$rawData[2]);
+					
+					$nRec = $keys[$pointer];
+					$hourMin = stringToTime('time',$nRec);
+					//print_r($hourMin);
+					$datetime = stringToTime('date',$rawData[0],$hourMin);
+					//print $datetime.'<br/>';
+					
+					$val = (float)str_replace(',','.',$rawData[$pointer]);
+					$cState = (int)$rawData[$pointer+1];
+					
+					$parsedData = array(
+						'id'=>$id,
+						'time'=>$datetime,
+						'type'=>$type,
+						'value'=>$val,
+						'cState'=>$cState
 					);
-			}		
-			array_push($data,$parsedData);
+					
+					array_push($data,$parsedData);
+				}
+				// if ($break) break;
+			}
 		}
-		// if ($break) break;
 		fclose($file);
-		
+		return $data;
 	}
-
 }
 
-parseData($path);
+
+
+// TEST EXECUTION
+
+?>
+
+<html>
+	<body>
+		<form action="#" method="post">
+			<input type="submit" name="clear_db"  value="clear db" />
+			<input type="submit" name="fill_db"  value="fill db" />
+		</form>
+	</body>
+</html>
+
+<?php
+
+if(isset($_POST['clear_db'])){
+	$query = "DELETE FROM record WHERE 1";
+	print $query.'<hr/>';
+	dbQuery($query);
+}else if(isset($_POST['fill_db'])){
+	$data = parseData($path);
+	print 'populating...<hr/>';
+	populateDb($data);
+}
 
 
 ?>
--- remote/app_logic/db_management.php
+++ remote/app_logic/db_management.php
@@ -2,16 +2,16 @@
 
 require_once("db_info.php");
 
+
+// DB
+
 function dbConnect(){
-
-$con = mysql_connect($dbHost,$dbUser,$dbPassword);
-if(!$con){ die('Could not connect: ' . mysql_error()); }
-
-mysql_select_db($dbName,$con);;
-
-echo 'Connected successfully';
-return $con;
-
+	
+	global $dbHost,$dbUser,$dbPassword, $dbName;
+	$con = mysql_connect($dbHost,$dbUser,$dbPassword, true);
+	$db = mysql_select_db($dbName,$con);
+	if(!$con){ die('Could not connect: ' . mysql_error()); }
+	return $con;
 }
 
 /*
@@ -21,15 +21,31 @@ function dbClose($con){
 */
 
 function dbQuery($q){
-
-	$con = $dbConnect();
-
-	mysql_query($q);
-	while($row = mysql_fetch_array()){
-		print $row;
+	$con = dbConnect();
+	$result = mysql_query($q, $con) or die (mysql_error());
+	$result_a = array();
+	while($row = mysql_fetch_assoc($result)){
+		$result_a[] = $row;
 	}
-	
 	mysql_close($con);
+	return $result_a;
+}
+
+
+// APP SPECIFIC
+
+function populateDb($data){
+	// $query = "SELECT id, time, obis, value, cs1 FROM record WHERE  id = 82042768 and not id = (select movieboard from MvMonitor where seq = '093')";
+	foreach($data as $d){
+		$id = $d['id'];
+		$time = $d['time'];
+		$obis = $d['type'];
+		$val = $d['value'];
+		$cs1 = $d['cState'];
+		$query = "INSERT INTO record (id, time, obis, value, cs1) VALUES ($id, '$time', '$obis', $val, $cs1)";
+		print $query.'<br/>';
+		//dbQuery($query);
+	}
 }
 
 ?>
